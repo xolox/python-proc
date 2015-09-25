d70aae66635c505f55549ef1171cace8eff982dc
Peter Odding <peter@peterodding.com>
2015-09-25 21:36:41 +0200

    Refactor setup.py script, improve trove classifiers

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-"""
-Setup script for the `proc` package.
-"""
+"""Setup script for the `proc` package."""
 
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: March 29, 2015
+# Last Change: September 25, 2015
 # URL: https://proc.readthedocs.org
 
+# Standard library modules.
+import codecs
 import os
-import setuptools
 import sys
+
+# De-facto standard solution for Python packaging.
+from setuptools import find_packages, setup
 
 # Find the directory where the source distribution was unpacked.
 source_directory = os.path.dirname(os.path.abspath(__file__))
@@ -25,9 +27,10 @@ from proc import __version__ as version_string
 # Fill in the long description (for the benefit of PyPI)
 # with the contents of README.rst (rendered by GitHub).
 readme_file = os.path.join(source_directory, 'README.rst')
-readme_text = open(readme_file, 'r').read()
+with codecs.open(readme_file, 'r', 'utf-8') as handle:
+    readme_text = handle.read()
 
-setuptools.setup(
+setup(
     name='proc',
     version=version_string,
     description="Simple interface to Linux process information",
@@ -35,11 +38,11 @@ setuptools.setup(
     url='https://proc.readthedocs.org',
     author='Peter Odding',
     author_email='peter@peterodding.com',
-    packages=setuptools.find_packages(),
-    test_suite='proc.tests',
+    packages=find_packages(),
     entry_points=dict(console_scripts=[
         'cron-graceful = proc.cron:main'
     ]),
+    test_suite='proc.tests',
     install_requires=[
         'cached-property >= 1.0.0',
         'coloredlogs >= 0.8',
@@ -47,15 +50,23 @@ setuptools.setup(
         'humanfriendly >= 1.16',
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Operating System Kernels :: Linux',
         'Topic :: System :: Systems Administration',
+        'Topic :: Terminals',
+        'Topic :: Utilities',
     ])
