f1f6e070e0af4ecebb947f5bb27d40aeb13786ff
Peter Odding <peter@peterodding.com>
2017-01-24 00:44:41 +0100

    Install gpg-agent on Travis CI (coverage!)

--- scripts/install-on-travis.sh
+++ scripts/install-on-travis.sh
@@ -1,19 +1,24 @@
 #!/bin/bash -e
 
-# This shell script is used by .travis.yml to install and configure the Apache
-# webserver and mod_wsgi in order to test the proc.apache module. This script
-# assumes it is running on Ubuntu 12.04 because that's what Travis CI uses at
-# the time of writing.
+# This shell script is used by .travis.yml:
+#
+#  - To install and configure the Apache webserver and
+#    mod_wsgi in order to test the proc.apache module.
+#
+#  - To install the gpg-agent in order to test the proc.gpg module.
+#
+# This script assumes it is running on Ubuntu 12.04 because
+# that's what Travis CI uses at the time of writing.
 
-# Let apt-get, dpkg and related tools know that we want the following commands
-# to be 100% automated (no interactive prompts).
+# Let apt-get, dpkg and related tools know that we want the following
+# commands to be 100% automated (no interactive prompts).
 export DEBIAN_FRONTEND=noninteractive
 
 # Update apt-get's package lists.
 sudo apt-get update -qq
 
-# Use apt-get to install the Apache webserver and mod_wsgi.
-sudo apt-get install --yes apache2-mpm-prefork libapache2-mod-wsgi
+# Use apt-get to install the Apache webserver, mod_wsgi and the GnuPG agent.
+sudo apt-get install --yes apache2-mpm-prefork libapache2-mod-wsgi gnupg-agent
 
 # Create a dummy virtual host that contains the minimal mod_wsgi directives
 # required to cause it to spawn daemon worker processes. Yes, this is a nasty
